652d19466f68d5f23c7ddc5ce72e2a2becb54302
Mike Rachman <mikerachman1@gmail.com>
2022-12-11 08:39:06 -0500

    sidebar div being populated with project names

--- src/sidebar.js
+++ src/sidebar.js
@@ -4,7 +4,14 @@ export default function generateSidebar(projects, currentProject) {
   const sidebarDiv = document.createElement('div');
   sidebarDiv.classList.add('sidebar');
 
-  //fill sidebar innerhtml with projects, new project button 
+  //fill sidebar innerhtml with projects, new project button
+  
+  projects.forEach(project => {
+    const projectDiv = document.createElement('div')
+    projectDiv.setAttribute('id', `project-${project.getId()}`);
+    projectDiv.innerHTML = project.getName();
+    sidebarDiv.appendChild(projectDiv);
+  });
 
   contentDiv.appendChild(sidebarDiv);
 }
