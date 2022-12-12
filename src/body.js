d8b83f2762ae1041ac3f262d4c1c225654ba2728
Mike Rachman <mikerachman1@gmail.com>
2022-12-12 11:50:55 -0500

    fixed bugs in editing tasks created by changing html structure of task divs

--- src/body.js
+++ src/body.js
@@ -8,83 +8,86 @@ export default function generateBody(currentProject) {
   const tasks = currentProject.getTasks();
 
   Object.values(tasks).forEach((task) => {
-     //change task div here
     str +=  `<div class='task-${task.id} task'>
-              <div class="task-main">
-                <div class='priority-${task.id} priority'></div>
-                <span class='task-details-${task.id}'>${task.title} - ${format((task.dueDate), 'M/dd/yy')}</span>
+              <div class="task-main-${task.id} task-main">
+                <div class="task-details-${task.id} task-details">
+                  <div class='priority-${task.id} priority'></div>
+                  <span class='task-title-${task.id}'>${task.title}</span>
+                  <span class='task-date-${task.id} task-date'>${format((task.dueDate), 'M/dd/yy')}</span>
+                </div>
                 <input type="checkbox" class='task-checkbox-${task.id}'>
               </div>
-              <div class="expanded-task-${task.id}">
-                <span class='task-description'>${task.description}</span>
+              <div class="expanded-task-${task.id} expanded-task">
+                <span class='task-description-${task.id} task-description'>${task.description}</span>
                 <span class='task-buttons-${task.id}'>
                   <button class='task-edit-${task.id}'>Edit</button>
                   <button class='task-delete-${task.id}'>Delete</button>
                 </span>
               </div>
             </div>`
-
     })
 
   bodyDiv.innerHTML = str;
 
-  
   Object.values(tasks).forEach((task) => {
     const checkBox = document.querySelector(`.task-checkbox-${task.id}`)
-    const taskDetails = document.querySelector(`.task-details-${task.id}`)
+    const taskTitle = document.querySelector(`.task-title-${task.id}`)
     const expandedTask = document.querySelector(`.expanded-task-${task.id}`)
     const deleteButton = document.querySelector(`.task-delete-${task.id}`)
     const taskDiv = document.querySelector(`.task-${task.id}`)
     const priorityDiv = document.querySelector(`.priority-${task.id}`)
+    const taskDetails = document.querySelector(`.task-details-${task.id}`)
+    const taskMain = document.querySelector(`.task-main-${task.id}`)
+    const taskDate = document.querySelector(`.task-date-${task.id}`)
+    const taskDescription = document.querySelector(`.task-description-${task.id}`)
     
     expandedTask.style.display = 'none'
     taskDetails.addEventListener('click', function() {
-      (expandedTask.style.display == 'none') ? expandedTask.style.display = 'block' : expandedTask.style.display = 'none'
+      (expandedTask.style.display == 'none') ? expandedTask.style.display = 'flex' : expandedTask.style.display = 'none'
     })
   
     if(task.completed) {
       checkBox.checked = true;
-       //change task div here
-      taskDetails.innerHTML = `<strike>${task.title} - ${format((task.dueDate), 'M/dd/yy')}</strike>`
+      taskTitle.innerHTML = `<strike>${task.title}</strike>`
     } else {
       checkBox.checked = false;
-       //change task div here
-      taskDetails.innerHTML = `${task.title} - ${format((task.dueDate), 'M/dd/yy')}`
+      taskTitle.innerHTML = `${task.title}`
     }
 
-    if (task.priority == 'high') {
-      priorityDiv.style.backgroundColor = '#d4142a'
-    } else if (task.priority == 'moderate') {
-      priorityDiv.style.backgroundColor = '#e8600a'
-    } else {
-      priorityDiv.style.backgroundColor = '#06b66c'
+    function priorityColorChange() {
+      if (task.priority == 'high') {
+        priorityDiv.style.backgroundColor = '#d4142a'
+      } else if (task.priority == 'moderate') {
+        priorityDiv.style.backgroundColor = '#e8600a'
+      } else {
+        priorityDiv.style.backgroundColor = '#06b66c'
+      }
     }
+    priorityColorChange();
 
     checkBox.addEventListener('click', function() {
       if (task.completed == false) {
         checkBox.checked = true;
         task.completed = true;
-         //change task div here
-        taskDetails.innerHTML = `<strike>${task.title} - ${format((task.dueDate), 'M/dd/yy')}</strike>`
+        taskTitle.innerHTML = `<strike>${task.title}</strike>`
       } else {
         checkBox.checked = false;
         task.completed = false;
-         //change task div here
-        taskDetails.innerHTML = `${task.title} - ${format((task.dueDate), 'M/dd/yy')}`
+        taskTitle.innerHTML = `${task.title}`
       }
-      // console.log(`${task.id}-${task.completed}`)
     })
 
     
     deleteButton.addEventListener('click', function() {
       taskDiv.style.display = 'none'
       currentProject.deleteTask(task.id)
-      // console.log(currentProject.getTasks())
     })
 
     const editButton = document.querySelector(`.task-edit-${task.id}`)
     editButton.addEventListener('click', function() {
-      taskDetails.innerHTML = `<form id="edit-task-${task.id}">
+      const editFormContainer = document.createElement('div');
+      taskDiv.appendChild(editFormContainer)
+      editFormContainer.innerHTML = `<form id="edit-task-${task.id}">
                             <label for="title">
                               <input type="text" name="title" id="title" value="${task.title}">
                             </label>
@@ -103,8 +106,10 @@ export default function generateBody(currentProject) {
                             </label>
                             <input type="submit" value="Submit">
                           </form>`
-      const taskButtons = document.querySelector(`.task-buttons-${task.id}`)
-      taskButtons.style.display = 'none'
+
+      taskMain.style.display = 'none'
+      expandedTask.style.display = 'none'
+
       const editTaskForm = document.querySelector(`#edit-task-${task.id}`)
       editTaskForm.addEventListener("submit", 
         function(event) {
@@ -120,9 +125,13 @@ export default function generateBody(currentProject) {
           task.dueDate = newDueDate;
           task.priority = newPriority;
 
-           //change task div here
-          (task.completed) ? taskDetails.innerHTML = `<strike>${task.title} - ${format((task.dueDate), 'M/dd/yy')}</strike>` : taskDetails.innerHTML = `${task.title} - ${format((task.dueDate), 'M/dd/yy')}`
-          taskButtons.style.display = 'inline'
+          (task.completed) ? taskTitle.innerHTML = `<strike>${task.title}</strike>` : taskTitle.innerHTML = `${task.title}`
+          taskDate.innerHTML = `${format((task.dueDate), 'M/dd/yy')}`;
+          taskDescription.innerHTML = newDescription;
+          priorityColorChange();
+
+          taskMain.style.display = 'flex'
+          editFormContainer.remove();
         })
     })
   })
