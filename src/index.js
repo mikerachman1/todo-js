5501377cf44d7b562b87c4fecbd24d04767d5879
Mike Rachman <mikerachman1@gmail.com>
2022-12-13 09:44:12 -0500

    wrapped index.js logic in intialize iife

--- src/index.js
+++ src/index.js
@@ -7,25 +7,26 @@ import { createBody, generateBody } from './body';
 import generateSidebar from './sidebar';
 
 //on first page load, create and set current project
-let projectCounter = 0;
-let projects = JSON.parse(localStorage.getItem('projects') || '[]');
-let currentProject;
-if (projects.length == 0) {
-  projects.push(projectBuilder(projectCounter++, 'My First Project'));
-  currentProject = projects[0];
-  const task1 = taskBuilder(currentProject.getCounter(), 'Hello! Click me to Expand.', 'This is your first task! Have fun!', new Date('December 17, 2023 03:24:00'), 'high')
-  currentProject.addTask(task1)
-} else {
-  projects = projects.map(proj => {
-    let newProj = projectBuilder(projectCounter++, proj.name, proj.tasks)
-    newProj.setCounter(proj.counter)
-    return newProj
-  })
-  currentProject = projects[JSON.parse(localStorage.getItem('currentProject'))]
-}
-
-generateHeader(currentProject);
-generateNewTaskModal(projects, currentProject);
-createBody();
-generateSidebar(projects, currentProject, projectCounter);
-generateBody(projects, currentProject);
+const initialize = (() => {
+  let projectCounter = 0;
+  let projects = JSON.parse(localStorage.getItem('projects') || '[]');
+  let currentProject;
+  if (projects.length == 0) {
+    projects.push(projectBuilder(projectCounter++, 'My First Project'));
+    currentProject = projects[0];
+    const task1 = taskBuilder(currentProject.getCounter(), 'Hello! Click me to Expand.', 'This is your first task! Have fun!', new Date('December 17, 2023 03:24:00'), 'high')
+    currentProject.addTask(task1)
+  } else {
+    projects = projects.map(proj => {
+      let newProj = projectBuilder(projectCounter++, proj.name, proj.tasks)
+      newProj.setCounter(proj.counter)
+      return newProj
+    })
+    currentProject = projects[JSON.parse(localStorage.getItem('currentProject'))]
+  }
+  generateHeader(currentProject);
+  generateNewTaskModal(projects, currentProject);
+  createBody();
+  generateSidebar(projects, currentProject, projectCounter);
+  generateBody(projects, currentProject);
+})();
